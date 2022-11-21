d4b2ea75801ceeadacb1c70950b9518fd6874af7
gregsantos <greg.santos@dapperlabs.com>
2022-11-20 19:41:44 -0800

    style: add theme, global css

--- theme.ts
+++ theme.ts
@@ -1,22 +1,36 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-const colors = {
-  brand: {
-    green: "#41FF00",
-    green2: "#5bf870",
-    test: "red",
-    800: "#153e75",
-    700: "#2a69ac",
+const styles = {
+  global: (props) => ({
+    body: {
+      backgroundColor: "red",
+    },
+    "*::placeholder": {
+      color: mode("gray.400", "whiteAlpha.400")(props),
+    },
+    "*, *::before, &::after": {
+      borderColor: mode("gray.200", "whiteAlpha.300")(props),
+      wordWrap: "break-word",
+    },
+  }),
+  colors: {
+    brand: {
+      green: "#41FF00",
+      green2: "#5bf870",
+      test: "red",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  },
+  fonts: {
+    heading: "Avenir Next, sans-serif",
+    body: "VT323, monospace",
+    mono: "VT323, monospace",
   },
 };
 
-const fonts = {
-  heading: "Avenir Next, sans-serif",
-  body: "VT323, monospace",
-  mono: "VT323, monospace",
-};
-
-const theme = extendTheme({ colors, fonts });
+const theme = extendTheme(styles);
 
 export default theme;
